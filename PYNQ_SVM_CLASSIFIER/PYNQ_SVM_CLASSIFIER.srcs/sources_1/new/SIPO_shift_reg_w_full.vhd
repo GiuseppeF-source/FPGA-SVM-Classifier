08429a8601308588a9caa52a65703499dd514f2a
GiuseppeF-source <z7giuseppe7z@gmail.com>
2022-10-20 22:58:00 +0200

    Inizio impl SIPO
    Creazione component SIPO_shift_reg_w_full
    manca gestione dell'output parallelo

---
+++ PYNQ_SVM_CLASSIFIER/PYNQ_SVM_CLASSIFIER.srcs/sources_1/new/SIPO_shift_reg_w_full.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity SIPO_shift_reg_w_full is
+  Generic (
+      n_in_bit  : natural := 32;
+      depth     : natural := 13;
+      n_out_bit : natural := 408
+  );
+  Port (
+    clk      : in std_logic;
+    rst      : in std_logic;
+    ce       : in std_logic;
+    data_in  : in std_logic_vector ( n_in_bit-1 downto 0);
+    data_out : out std_logic_vector ( n_out_bit-1 downto 0);
+    full     : out std_logic
+   );
+end SIPO_shift_reg_w_full;
+
+architecture rtl of SIPO_shift_reg_w_full is
+
+type shift_reg_t is array (0 to depth - 1) of std_logic_vector(data_in'range);
+signal  shift_reg : shift_reg_t;
+
+signal count : natural range 0 to depth+1 :=0;
+
+begin
+
+shift_p: process ( clk ) 
+begin 
+    if rising_edge(clk) then 
+        if rst = '1' then
+            count <= 0;
+        else
+            if ce = '1' then
+                count <= count + 1;
+    transf_loop:
+                for i in 1 to depth-1 loop
+                    shift_reg(i) <= shift_reg( i - 1 ); -- shift dei 32 bit 
+                end loop;
+                    shift_reg(0) <= data_in;
+            end if;
+        end if;
+    end if;
+
+end process;
+
+full <= '1' when ( count = 13 ) else '0';
+
+end rtl;
